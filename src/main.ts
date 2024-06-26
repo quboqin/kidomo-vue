ec27c8f6eefedd8ca53d73e63acbffb01e77efd2
Qubo Qin <quboqin0710@gmail.com>
2024-06-26 21:08:58 +0800

    create web bridge as a plugin

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,14 @@ import '@/assets/tailwind.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import { systemInfoStore } from '@/stores'
 import App from './App.vue'
 import router from './router'
-import './utils/web_bridge'
+import WebBridge from './utils/web_bridge'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-function callNativeFunction(jsonObject: any) {
-  const sysInfoStore = systemInfoStore()
-  const os = sysInfoStore.getOperatingSystem()
-  jsonObject['callback'] = os === 'Android' ? 'callbackFromKotlin' : 'callbackFromSwift'
-  if (os === 'Android') {
-    Android.callFromJavascript(JSON.stringify(jsonObject))
-  } else if (os === 'iOS') {
-    window.webkit.messageHandlers.Callback.postMessage(jsonObject)
-  }
-}
-
-app.config.globalProperties.$callNativeFunction = callNativeFunction
-
-// set header
-;(function () {
-  const header = {
-    action: 'set_header',
-    auth: {
-      BladeAuth: 'bladeAuth_xxx',
-      Authorization: 'authorization_xxx'
-    }
-  }
-  callNativeFunction(header)
-})()
+app.use(WebBridge)
 
 app.mount('#app')
--- src/utils/web_bridge.ts
+++ src/utils/web_bridge.ts
@@ -1,3 +1,7 @@
+import type { App } from 'vue'
+
+import { systemInfoStore } from '@/stores'
+
 window.callbackFromKotlin = function (jsonObject: any) {
   alert('callbackFromKotlin')
 }
@@ -9,3 +13,48 @@ window.callbackFromSwift = function (jsonObject: any) {
 function showActionSheet() {
   Android.showActionSheet(true)
 }
+
+class WebBridge {
+  // Method to call native function
+  callNativeFunction(jsonObject: any): void {
+    const sysInfoStore = systemInfoStore()
+    const os = sysInfoStore.getOperatingSystem()
+    jsonObject['callback'] = os === 'Android' ? 'callbackFromKotlin' : 'callbackFromSwift'
+    if (os === 'Android') {
+      Android.callFromJavascript(JSON.stringify(jsonObject))
+    } else if (os === 'iOS') {
+      window.webkit.messageHandlers.Callback.postMessage(jsonObject)
+    }
+  }
+
+  // Method to set header
+  setHeader() {
+    const header = {
+      action: 'set_header',
+      auth: {
+        BladeAuth: 'bladeAuth_xxx',
+        Authorization: 'authorization_xxx'
+      }
+    }
+    this.callNativeFunction(header)
+  }
+
+  // Plugin installation method
+  static install(app: App) {
+    // Create an instance of WebBridge
+    const webBridge = new WebBridge()
+
+    // Make WebBridge instance available to the whole app
+    app.config.globalProperties.$webBridge = webBridge
+
+    // Optionally, provide the WebBridge instance through provide/inject mechanism
+    app.provide('webBridge', webBridge)
+
+    app.config.globalProperties.$callNativeFunction = webBridge.callNativeFunction.bind(webBridge)
+
+    webBridge.setHeader()
+  }
+}
+
+// Export WebBridge as a plugin
+export default WebBridge
